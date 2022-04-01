3c36caf64d800f64373e13032fdf60e985502638
Buco <arnaud@grimbert.com>
2022-04-01 19:38:00 +0200

    add subscribe command

--- cogs/insitute.py
+++ cogs/insitute.py
@@ -1,13 +1,12 @@
-import discord
 from discord.ext import commands
 from discord import app_commands
+from utils.modals import TransactionModal
 
 
 class Institute(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @app_commands.command(name="subscribe")
-    async def subscribe_(self):
-        
+    async def subscribe_(self, interaction):
+        await interaction.response.send_modal(TransactionModal)
