f0061f7d7d013ece20a9314ed0f208920510d175
Buco <arnaud@grimbert.com>
2022-04-01 18:07:55 +0200

    add .gitignore

--- private/config.py
+++ private/config.py
@@ -7,4 +7,4 @@ DEFAULT_PREFIXES = ('akb-',)
 LOCAL_USER = 'Buco'
 PREVENT_LOCAL_COMMANDS = True
 _db_conf = namedtuple('db_conf', ['host', 'port', 'user', 'password', 'db'])
-DB_CONF = _db_conf(host='127.0.0.1', port=5432, user='arnaud', password='Dbboude1!', db='akb')
+DB_CONF = _db_conf(host='127.0.0.1', port=5432, user='arnaud', password='Psqlaboude1!', db='akb')
